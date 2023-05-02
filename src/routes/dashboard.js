51c391c62944b7cc27c133d391f44df447443d5f
Emir OĞUZ <ranorkk@gmail.com>
2023-05-02 17:44:11 +0300

    dashboard -> route update

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,8 +1,11 @@
+import { PrismaClient } from '@prisma/client'
 import { Response } from "../libraries/response.js";
 import { Current, CurrentActivity } from "../models/current.js";
 import { Order, OrderItem } from "../models/order.js";
 import { Task } from "../models/task.js";
 import { Route } from "./route.js";
+
+const prisma = new PrismaClient()
 
 export class Route_Dashboard extends Route {
 
@@ -17,16 +20,15 @@ export class Route_Dashboard extends Route {
 
   async get (res, user, body = {}) {
 
+    let monthly_total_sales = await prisma.$queryRaw`SELECT (EXTRACT(MONTH FROM date)) AS month, (EXTRACT(YEAR FROM date)) AS year, SUM(balance) as balance FROM "CurrentActivity" WHERE balance > 0 GROUP BY (EXTRACT(MONTH FROM date)), (EXTRACT(YEAR FROM date)) ORDER BY (EXTRACT(YEAR FROM date)), (EXTRACT(MONTH FROM date))`
+
     let content = {
       active_task_count: await Task.count({ where: { state: "Aktif" } }),
       not_created_task_count: await Order.count({ where: { task: null } }),
       overdue_task_count: await Task.count({ where: { current_step: { planned_finish_date: { lt: new Date() } }, state: "Aktif" } }),
       complated_order_count_month: await Task.count({ where: { finish_date: { gt: new Date(new Date().getFullYear(), new Date().getMonth(), 1) }}}),
       active_tasks: await Task.getMany({ where: { state: "Aktif", closed: false }}),
-      sales_data_month: (await CurrentActivity.getMany({
-        where: { balance: { gt: 0 } },
-        select: { registry_date: true, balance: true }
-      })).map(r => {r = r.details; r.cumulative_balance = undefined; return r}),
+      sales_data_month: monthly_total_sales,
       current_final_balances: (await Current.getFinalBalances())
     }
 
